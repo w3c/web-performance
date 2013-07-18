629ce0de42e704e0664149cdd5440176155be828
James Simonsen <simonjam@chromium.org>
2013-07-17 17:15:10 -0700

    Add 2 new Resource Timing tests and fix up a couple others.

--- tests/resources/blank_page_green_with_allow_timing.php
+++ tests/resources/blank_page_green_with_allow_timing.php
@@ -1,12 +1,12 @@
+<?php
+  $origin = $_GET["origin"] ? $_GET["origin"] : "";
+  header("timing-allow-origin: $origin");
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
         <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
         <title>Green Test Page</title>
-        <?php
-          $origin = $_GET["origin"] ? $_GET["origin"] : "";
-          header("timing-allow-origin: $origin");
-        ?>
     </head>
     <body style="background-color:#00FF00;">
         <h1>Placeholder</h1>
