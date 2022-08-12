76737c5e1ce968e325f9ee7d3c7d3ca8878823f6
Yoav Weiss <yoav@yoav.ws>
2022-08-12 13:31:52 +0200

    Minutes for Aug 4th and cleaner updates

--- meetings/cleaner.sh
+++ meetings/cleaner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat $1 | sed 's/\([>;]\)\([a-zA-Z ]*\): /\1<b>\2<\/b>: /g' | sed 's/Updated automatically every 5 minutes//' | sed 's/\([Nn]\)icolas/\1icol\&aacute;s/g' | sed 's/Pena/Pe\&ntilde;a/g' | sed 's/Published by//'| sed 's/Google Drive//g' | sed 's/Report Abuse//' | sed 's/div id="publish-banner"/div style="display:none" id="publish-banner"/'  > /tmp/tmp_file.html
+cat $1 | sed 's/\([>;]\)\([a-zA-Z ]*\): /\1<b>\2<\/b>: /g' | sed 's/Updated automatically every 5 minutes//' | sed 's/\([Nn]\)icolas/\1icol\&aacute;s/g' | sed 's/Pena/Pe\&ntilde;a/g' | sed 's/Published by//'| sed 's/Google Drive//g' | sed 's/Report Abuse//' | sed 's/div id="publish-banner"/div style="display:none" id="publish-banner"/' | sed 's/padding:72pt 72pt 72pt 72pt//' | sed 's/padding: 100px 20% 50px 20%/padding: 50px 5% 50px 5%/' | sed 's/max-width:468pt/max-width:600pt/' > /tmp/tmp_file.html
 mv /tmp/tmp_file.html $1
